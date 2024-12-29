13b6e1ce7353c6a984592142961fe12e4bcf2282
proDream <sushkoos@gmail.com>
2024-12-29 22:43:36 +0400

    feat: add new modules and update settings structure
    - Add 'elements' and 'backup_params' modules to organize code better.
    - Update 'settings.rs' to include S3 access and secret keys, and temporary directory.
    - Modify 'Settings' struct to use public fields for S3 access and secret keys.
    - Update 'from_file' method in 'Settings' to handle errors more gracefully.

--- src/structures/mod.rs
+++ src/structures/mod.rs
@@ -1,1 +1,3 @@
 pub mod settings;
+mod elements;
+mod backup_params;
--- src/structures/settings.rs
+++ src/structures/settings.rs
@@ -1,30 +1,28 @@
-use std::io;
 use serde::Deserialize;
-use crate::utils;
+use std::{fs, io};
+use crate::structures::elements::Elements;
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     pub s3_endpoint: String,
     pub s3_region: String,
     pub s3_bucket: String,
-    elements: Vec<Elements>,
+    pub s3_access: String,
+    pub s3_secret: String,
+    pub temp_dir: String,
+    pub elements: Vec<Elements>,
 }
 
-#[derive(Debug, Deserialize)]
-struct Elements {
-    s3_folder: String,
-    dest_type: String,
-    backup_retention_days: u64,
-    s3_backup_retention_days: u64,
-}
 
 impl Settings {
-    pub fn new() -> io::Result<Settings> {
-        let settings: Settings = match utils::json_utils::read_json() {
+    pub fn from_file() -> io::Result<Settings> {
+        let file_content = fs::read_to_string("settings.json")?;
+
+        let settings: Settings = match serde_json::from_str(&file_content) {
             Ok(data) => data,
             Err(err) => {
-                eprintln!("Error reading JSON file: {}", err);
-                return Err(err);
+                eprintln!("Error parsing JSON file: {}", err);
+                return Err(io::Error::new(io::ErrorKind::InvalidData, err));
             }
         };
 
