e23077b89181f1bab0f5184371943e5fe4ccb71e
proDream <sushkoos@gmail.com>
2024-12-30 13:34:26 +0400

    feat: add command-line argument handling in main.rs
    - Added `use std::env;` to import environment variables.
    - Collected command-line arguments into a vector.
    - Checked if at least one argument is provided, printing an error message and exiting if not.
    - Modified the `main` function to handle a single command argument (`backup`) for initiating the backup process.
    - Added a check for unknown command arguments, printing an error message and exiting if encountered.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use crate::structures::settings::Settings;
 use crate::utils::process_backup::start_backup_process;
+use std::env;
 
 mod structures;
 mod utils;
 
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("No command argument provided. Exiting.");
+        return;
+    }
+
     let settings = match Settings::from_file() {
         Ok(s) => s,
         Err(err) => {
@@ -22,5 +30,13 @@ async fn main() {
         }
     };
 
-    start_backup_process(&settings, &bucket).await;
+    match args[1].as_str() {
+        "backup" => {
+            start_backup_process(&settings, &bucket).await;
+        }
+        _ => {
+            eprintln!("Unknown argument provided. Exiting.");
+            return;
+        }
+    }
 }
