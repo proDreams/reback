24f738807724bc08c2045464dba129c87b6db37b
proDream <sushkoos@gmail.com>
2024-12-30 12:00:37 +0400

    feat(src/structures/elements.rs): add file path and backup command for PostgreSQL
    - Introduce `chrono::Local` for timestamp generation
    - Add `std::path::{Path, PathBuf}` and `std::process::Command` for file handling and external command execution
    - Modify `perform_backup` method to accept a `&Path` parameter and return a `Result<PathBuf, String>`
    - Implement PostgreSQL backup logic using Docker exec and pg_dump
    - Update MongoDB backup logging to include `Debug` formatting

--- src/structures/elements.rs
+++ src/structures/elements.rs
@@ -1,10 +1,12 @@
 use crate::structures::backup_params::BackupParams;
+use chrono::Local;
 use serde::Deserialize;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 #[derive(Debug, Deserialize)]
 pub struct Elements {
     pub element_title: String,
-    pub element_type: String,
     pub s3_folder: String,
     pub backup_retention_days: u64,
     pub s3_backup_retention_days: u64,
@@ -12,7 +14,8 @@ pub struct Elements {
 }
 
 impl Elements {
-    pub async fn perform_backup(&self) -> Result<(), String> {
+    pub async fn perform_backup(&self, path: &Path) -> Result<PathBuf, String> {
+        let mut file_path: PathBuf = Default::default();
         match &self.params {
             Some(BackupParams::Postgresql {
                 db_host,
@@ -36,13 +39,39 @@ impl Elements {
                     "Backing up PostgreSQL Docker: docker_container={}, db={}, user={}",
                     docker_container, db_name, db_user
                 );
+
+                let now = Local::now().format("%Y-%m-%d_%H-%M-%S").to_string();
+
+                let file_name = format!("{}-{}.sql", self.element_title, now);
+                file_path = path.join(&file_name);
+
+                let command = format!(
+                    "docker exec {} bash -c \"PGPASSWORD='{}' pg_dump -U {} {}\" > {}",
+                    docker_container,
+                    db_password,
+                    db_user,
+                    db_name,
+                    file_path.display(),
+                );
+                let output = Command::new("sh")
+                    .arg("-c")
+                    .arg(command)
+                    .output()
+                    .expect("Failed to execute backup command");
+
+                if output.status.success() {
+                    println!("Backup PostgreSQL Docker created successfully!");
+                } else {
+                    eprintln!("Backup failed!");
+                    eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
+                }
             }
             Some(BackupParams::Mongodb {
                 db_name,
                 db_user,
                 db_password,
             }) => {
-                println!("Backing up MongoDB: db={}, user={}", db_name, db_user);
+                println!("Backing up MongoDB: db={:?}, user={:?}", db_name, db_user);
             }
             Some(BackupParams::MongodbDocker {
                 docker_container,
@@ -51,7 +80,7 @@ impl Elements {
                 db_password,
             }) => {
                 println!(
-                    "Backing up MongoDB: docker_container={}, db={}, user={}",
+                    "Backing up MongoDB: docker_container={}, db={:?}, user={:?}",
                     docker_container, db_name, db_user
                 );
             }
@@ -65,6 +94,6 @@ impl Elements {
                 ));
             }
         }
-        Ok(())
+        Ok(file_path)
     }
 }
